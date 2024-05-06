f6244a23d7f9449846982801106d6554b17a7807
meowtec <bertonzh@gmail.com>
2024-05-06 19:28:50 +0800

    Fix build error

--- packages/hash-tool-online/vite.config.js
+++ packages/hash-tool-online/vite.config.js
@@ -1,20 +1,27 @@
 // @ts-check
 import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
+import react from '@vitejs/plugin-react';
 import wasm from 'vite-plugin-wasm';
 import topLevelAwait from 'vite-plugin-top-level-await';
 
-export default defineConfig(({ command }) => ({
-  root: 'src',
-  build: {
-    outDir: '../../..',
-  },
-  base: 'https://meowtec.github.io/hash-tool/',
-  plugins: [preact(), wasm(), topLevelAwait()],
-  resolve: {
-    alias: {},
-  },
-  worker: {
-    plugins: () => [wasm(), topLevelAwait()],
-  },
-}));
+export default defineConfig(({ command }) => {
+  const isBuild = command === 'build';
+
+  return {
+    build: {
+      outDir: isBuild ? '../../..' : undefined,
+    },
+    base: isBuild ? 'https://meowtec.github.io/hash-tool/' : undefined,
+    plugins: [isBuild ? preact() : react(), wasm(), topLevelAwait()],
+    resolve: {
+      alias: {},
+    },
+    worker: {
+      plugins: () => [wasm(), topLevelAwait()],
+    },
+    server: {
+      port: 11211,
+    },
+  };
+});
