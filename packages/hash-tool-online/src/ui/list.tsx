ab28c933fec1725b498c7cd9182a2ed99f53f0a7
meowtec <bertonzh@gmail.com>
2025-02-12 23:50:24 +0800

    Improve UI

--- packages/hash-tool-online/src/ui/list.tsx
+++ packages/hash-tool-online/src/ui/list.tsx
@@ -1,6 +1,8 @@
 import { useAppStore } from '../store';
 import { HashView } from './components/hash';
 
+const numberFormat = new Intl.NumberFormat('en-US', { maximumSignificantDigits: 3 });
+
 export default function List() {
   const { hashesTasks, options, removeTask } = useAppStore();
 
@@ -8,22 +10,33 @@ export default function List() {
     <div className="task-list">
       {hashesTasks.map((task) => (
         <div key={task.id} className="task-item">
-          <div className="hash-pair">
+          <div className="hash-kv">
             {typeof task.content === 'string' ? (
               <>
                 <div className="hash-label">text:</div>
-                {task.content}
+                <div className="hash-value">{task.content}</div>
               </>
             ) : (
               <>
                 <div className="hash-label">file:</div>
-                {task.content.name}
+                <div className="hash-value">{task.content.name}</div>
               </>
             )}
           </div>
+          {typeof task.content === 'string' ? (
+            <div className="hash-kv">
+              <div className="hash-label">length:</div>
+              <div className="hash-value">{numberFormat.format(task.content.length)}</div>
+            </div>
+          ) : (
+            <div className="hash-kv">
+              <div className="hash-label">size:</div>
+              <div className="hash-value">{numberFormat.format(task.content.size)} bytes</div>
+            </div>
+          )}
 
           {options.hashTypes.map((type) => (
-            <div key={type} className="hash-pair">
+            <div key={type} className="hash-kv">
               <div className="hash-label">{type}:</div>
               <HashView hashState={task.hashes[type] ?? null} />
             </div>
